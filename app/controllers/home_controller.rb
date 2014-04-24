b9e23f99e54c0893c40188b00ff89a4c5fd1ca47
rails <rails@rails-dev.(none)>
2014-04-24 11:12:55 +0530

    add ajax, feature in mid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
   def index
     @quizes = Quize.publish.order("created_at DESC")
   end
-  
+
+  def score_up
+    render :text => 'thanks'
+  end
+    
 end
